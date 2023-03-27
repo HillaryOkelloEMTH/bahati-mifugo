00c816576b931adf3ecc6f18deb9b39ae81b75a9
petershivachi <shitsukanepeter@gmail.com>
2023-03-27 13:41:54 +0300

    b2c callback

--- src/main/java/com/emtech/dairyapp/Transactions/Mpesa/MpesaService.java
+++ src/main/java/com/emtech/dairyapp/Transactions/Mpesa/MpesaService.java
@@ -231,7 +231,7 @@ public class MpesaService {
 
     public void processSTKPushCallBack(@NonNull Object object){
         Gson gson = new Gson();
-        log.log(Level.INFO, "B2C Callback received at " + new Date());
+        log.log(Level.INFO, "STK Callback received at " + new Date());
         JSONObject j1 = new JSONObject(gson.toJson(object));
 
         log.log(Level.INFO, String.format("STK Init Callback Response %s", j1));
