3bf83f537162f53a0f04b67fa169cfabc2f408c0
jmngige <jamesmngige@gmail.com>
2023-03-31 10:14:12 +0300

    milk colelction status updates

--- src/main/java/com/emtech/dairyapp/Dairy/Supply/MilkCollectionService.java
+++ src/main/java/com/emtech/dairyapp/Dairy/Supply/MilkCollectionService.java
@@ -289,6 +289,17 @@ public class MilkCollectionService {
         return response;
     }
 
+    public List<CollectionsData> fetchCollectorsCollectionsHistoryByDateRangeAndPaymentStatus(Long collectorId, String from, String to, Character paymentStatus) {
+        try {
+
+            return milkCollectionRepo.fetchCollectorsCollectionsHistoryByDateRangeAndPaymentStatus(collectorId, from, to, paymentStatus);
+
+        }catch (Exception exception){
+            log.info("Fetching Collectors Collection Response " + exception.getLocalizedMessage());
+            return null;
+        }
+    }
+
     public EntityResponse getCollectionsByColelctor(Long collectorId) {
 
         EntityResponse response = new EntityResponse();
@@ -547,8 +558,6 @@ public class MilkCollectionService {
         }
         return response;
     }
-
-
 }
 
 
