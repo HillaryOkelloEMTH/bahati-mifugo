00c5e146694a84e091c9557d8b1b29bb000110e1
Rubyroy12 <ibrakipz7@gmail.com>
2023-04-19 11:12:00 +0300

    get routes by colector

--- src/main/java/com/emtech/dairyapp/Configurations/Routes/RouteRepo.java
+++ src/main/java/com/emtech/dairyapp/Configurations/Routes/RouteRepo.java
@@ -14,6 +14,13 @@ public interface RouteRepo extends JpaRepository<Route,Long> {
     @Query(value = "select * from route",nativeQuery = true)
     List<Route> getRoutes();
 
+    @Query(nativeQuery = true,value = "SELECT r.route,p.name as pickUpLocation from route r join pick_up_locations p on p.id =r.location_id join collector c  on p.id =c.location_id  join users u on u.user_name =c.username where u.id =:collectorId")
+    List<CollectorRoutes> getRouteByCollector(Long collectorId);
+    interface CollectorRoutes{
+        String getRoute();
+        String getPickUpLocation();
+    }
+
 
 
 
