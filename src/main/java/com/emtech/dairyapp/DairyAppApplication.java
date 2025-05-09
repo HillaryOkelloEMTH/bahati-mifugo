a0e518ec4b28bfbcbf50ee3485eb0db63903f301
michaelndirangu <ndirangumichael33@gmail.com>
2025-05-09 17:02:49 +0300

    Set system timezone to Africa/Nairobi. 09/05/25

--- src/main/java/com/emtech/dairyapp/DairyAppApplication.java
+++ src/main/java/com/emtech/dairyapp/DairyAppApplication.java
@@ -28,6 +28,9 @@ import java.util.*;
 public class DairyAppApplication {
 
     public static void main(String[] args) {
+        System.setProperty("user.timezone", "Africa/Nairobi");
+        TimeZone.setDefault(TimeZone.getTimeZone("Africa/Nairobi"));
+
         SpringApplication.run(DairyAppApplication.class, args);
         Date currentDate = Calendar.getInstance(TimeZone.getDefault()).getTime();
         log.info("Current Local Date for Africa/Nairobi Zone is :: {} ::", currentDate);
